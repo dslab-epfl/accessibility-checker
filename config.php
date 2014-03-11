1e471a3ecd79c27342c238f96c5b1f50f44db33d
QuentinC <webmaster@quentinc.net>
2014-03-11 10:11:59 +0100

    Working copy of moodle 2.6.2 fresh

--- config.php
+++ config.php
@@ -18,10 +18,25 @@ $CFG->dboptions = array (
 );
 
 $CFG->wwwroot   = 'http://localhost/moodle';
-$CFG->dataroot  = 'C:\\wamp\\moodle-data';
+$CFG->dataroot  = 'C:\\wamp\\www\\moodle-data';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
+
+$CFG->admineditalways = true;
+$CFG->filelifetime = 15;
+$CFG->disableupdatenotifications = true;
+$CFG->disableupdateautodeploy = true;
+
+@error_reporting(E_ALL | E_STRICT);
+@ini_set('display_errors', '1');
+$CFG->debug = E_ALL &~ E_STRICT;
+$CFG->dblogerror = true;
+$CFG->debugdisplay = 1;
+$CFG->cachejs = false;
+//$CFG->noemailever = true;
+$CFG->divertallemailsto = 'quentin.cosendey@epfl.ch';
+
 
 require_once(dirname(__FILE__) . '/lib/setup.php');
 
